6b5e44662aff76972bef66141b1e1d5c6586ba5c
Murilo Santos <mucrilobass@gmail.com>
2022-05-02 19:08:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/requisicaohttpasynctask/Tarefa.java
@@ -0,0 +1,10 @@
+package com.example.requisicaohttpasynctask;
+
+import android.os.AsyncTask;
+
+public class Tarefa extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
