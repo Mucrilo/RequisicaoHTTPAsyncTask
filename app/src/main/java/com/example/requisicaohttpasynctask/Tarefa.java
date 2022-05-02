fa7ca742084b71e7933672aad787cd36c2263e6b
Murilo Santos <mucrilobass@gmail.com>
2022-05-02 19:11:50 -0400

    Interface criada

--- app/src/main/java/com/example/requisicaohttpasynctask/Tarefa.java
+++ app/src/main/java/com/example/requisicaohttpasynctask/Tarefa.java
@@ -1,10 +1,18 @@
 package com.example.requisicaohttpasynctask;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 
-public class Tarefa extends AsyncTask {
+public class Tarefa extends AsyncTask <String, Void, Bitmap>{
+
+    private Context context;
+    private ProgressDialog dialog;
+
+
     @Override
-    protected Object doInBackground(Object[] objects) {
+    protected Bitmap doInBackground(String... strings) {
         return null;
     }
 }
---
+++ app/src/main/java/com/example/requisicaohttpasynctask/TarefaInterface.java
@@ -0,0 +1,8 @@
+package com.example.requisicaohttpasynctask;
+
+import android.graphics.Bitmap;
+
+public interface TarefaInterface {
+
+    public void depoisDownload(Bitmap bitmap);
+}
